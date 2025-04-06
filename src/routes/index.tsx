c9bc88eb3bd75e0ad06fb27f3231a515f5ded9a5
유태양 <taeyang.yu@autowini.com>
2025-04-06 10:55:39 +0900

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <p class="text-red-500">hihihi</p>
+      <button type="button" class="btn btn-error">
+        hihi
+      </button>
+    </main>
+  );
+}
