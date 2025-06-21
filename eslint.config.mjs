81a102d944ad57f20a2f5e308d46228bc9b39ce7
tyange <usun16@gmail.com>
2025-06-21 16:34:42 +0900

    new linter option with antfu

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
+import antfu from '@antfu/eslint-config'
+
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  antfu({
+    typescript: true,
+  }),
 )
