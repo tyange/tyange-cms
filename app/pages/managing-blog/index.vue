35b0fffd141bc836356a833ddd931a569dcf23e5
tyange <usun16@gmail.com>
2026-01-20 07:45:29 +0900

    server false for debugging

--- app/pages/managing-blog/index.vue
+++ app/pages/managing-blog/index.vue
@@ -12,6 +12,7 @@ const authObject = useCookie<AuthStore>('auth')
 
 const { data, refresh } = await useFetch<CMSResponse<{ posts: PostListItem[] }>>(`/api/posts`, {
   headers: { Authorization: authObject.value!.accessToken! },
+  server: false,
 })
 
 const postList = computed(() => data.value?.data.posts ?? [])
