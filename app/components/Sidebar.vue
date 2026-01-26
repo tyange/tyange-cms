077c6bd9b873bf7dbf834b8cebad5bf5789083a8
tyagne <usun16@gmail.com>
2026-01-26 13:06:15 +0900

    Sidebar에 메뉴 추가

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -6,7 +6,12 @@ defineEmits<{ (e: 'handleCloseDrawer'): void }>()
   <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
     <li>
       <NuxtLink to="/managing-blog" @click="$emit('handleCloseDrawer')">
-        블로그 관리
+        포스트 목록
+      </NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/managing-blog/create" @click="$emit('handleCloseDrawer')">
+        새 글 쓰기
       </NuxtLink>
     </li>
   </ul>
